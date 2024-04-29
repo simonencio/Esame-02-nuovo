eb0a18decec0261f309f2a0441c623a61d16b0fd
Simone <simone2nencioni@icloud.com>
2024-04-29 10:53:06 +0200

    .

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
-    <link rel="stylesheet" href="./css/STYLE.min.css">
+    <link rel="stylesheet" href="./CSS/STYLE.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
@@ -21,20 +21,20 @@
         <div class="title">
             <h1>Esplora Insieme A Noi</h1>
         </div>
-        <section class="fotolavori">
+        <div class="Home">
             <div class="card">
                 <h4>CHI SIAMO?</h4>
-            <a href="./chisiamo.php" target="_blank"><img src="./immagini/pagina-chi-siamo.jpg" alt="pagina-chi-siamo" title="pagina-chi-siamo"></a>
+            <a href="./chisiamo.php" target="_blank" title="paginaChiSiamo"><img src="./immagini/pagina-chi-siamo.jpg" alt="pagina-chi-siamo" title="pagina-chi-siamo"></a>
             </div>
             <div class="card">
                 <h4>PORTFOLIO</h4>
-            <a href="./portfolio.php" target="_blank"><img src="./immagini/portfolio.jpg" alt="portfolio" title="portfolio"></a>
+            <a href="./portfolio.php" target="_blank"title="paginaPortfolio"><img src="./immagini/portfolio.jpg" alt="portfolio" title="portfolio"></a>
             </div>
             <div class="card">
                 <h4>CONTATTI</h4>
-            <a href="./contatti.php" target="_blank"><img src="./immagini/contattaci.jpg" alt="contatti" title="contatti"></a>
+            <a href="./contatti.php" target="_blank"title="paginaContatti"><img src="./immagini/contattaci.jpg" alt="contatti" title="contatti"></a>
             </div>
-        </section>
+</div>
     </main>
     <?php include 'footer.php';
         echo generateFooter();
